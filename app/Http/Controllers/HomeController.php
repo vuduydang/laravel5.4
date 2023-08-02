46b141c443c6302cc6f19415e165430e17ae47eb
DangVD <vuduydang2000@gmail.com>
2023-08-02 23:25:48 +0700

    first commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        dd(Storage::disk('local')->listContents('zip-downloads', true)) ;
+
     }
 }
